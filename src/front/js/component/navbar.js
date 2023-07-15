b1911df56070b4a7fbfa07c4d4fdd750ecf2cb1b
Kochu26 <josuefallasm26@hotmail.com>
2023-07-15 05:13:35 +0000

    cambios

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">
+            Aqui va el logo de la pagina
+          </span>
+        </Link>
+        <button className="btn btn-primary">button</button>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-primary">
+              Aqui iria el icono de hamburguesa
+            </button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
+
